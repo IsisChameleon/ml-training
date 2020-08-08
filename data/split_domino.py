10b51ced6bf01afad98b1f3e05c3b9a90c1c2af2
Isis <isisdesade@gmail.com>
2020-08-08 16:57:45 +1000

    Pix2Pix databunch ready!

--- data/split_domino.py
+++ data/split_domino.py
@@ -3,7 +3,7 @@ import logging
 import sys, os
 from argparse import ArgumentParser
 from pathlib import Path
-from PIL import Image
+from PIL import Image, JpegImagePlugin
 from fastai.data_block import get_files
 
 
@@ -21,22 +21,47 @@ def load_images(path, size=(256,512)):
 		tar_list.append(map_img)
 	return [asarray(src_list), asarray(tar_list)]
 
+def split_image(imgPath: Path):
+    try:
+        im = Image.open(imgPath)
+        w, h = im.size
+        im1 = im.crop((0, 0, w/2, h))
+        im2 = im.crop((w/2, 0, w, h))
+        return (im, im1, im2)
+
+    except BaseException as e:
+        print(f"Exception {e} occured")
+        logger.error(f"Exception {e} occured")
+
+def save_image(img: Image, original_img: Image, fp: Path):
+    # How to save an image with same quality as original? https://stackoverflow.com/questions/4354543/determining-jpg-quality-in-python-pil#4355281
+    # https://jdhao.github.io/2019/07/20/pil_jpeg_image_quality/
+    # https://pillow.readthedocs.io/en/stable/reference/JpegPresets.html
+    # http://code.nabla.net/doc/PIL/api/PIL/PIL.JpegImagePlugin.html
+    frmt = original_img.format
+
+    if frmt == 'JPEG':
+        quantization = getattr(original_img, 'quantization', None)
+        subsampling = JpegImagePlugin.get_sampling(original_img)
+        img.save(fp, format=frmt, subsampling=subsampling, qtables=quantization)
+    else:
+        img.save(fp, format=frmt)
+    pass
 
 def split_domino(domino_loc:str, list_subfolders=['train', 'val']):
 
-    logger.info(f'Starting split_domino to split images in ${domino_loc} for folders ${list_subfolders}')
+    logging.info(f'Starting split_domino to split images in ${domino_loc} for folders ${list_subfolders}')
 
     for sf in list_subfolders:
         current_path=Path(domino_loc)/sf
-        # os.mkdir(str(current_path/'Xs'))
-        # os.mkdir(str(current_path/'Ys'))
-        print(f"Directory to create {current_path/'Xs'}")
-        print(f"Directory to create {current_path/'Ys'}")
+
+        Path(domino_loc,'Xs',sf).mkdir(parents=True, exist_ok=True)
+        Path(domino_loc,'Ys',sf).mkdir(parents=True, exist_ok=True)
+
         for file in get_files(path=current_path, extensions=['.jpg'], recurse=False ):
-            print(f"File to split ${file}")
-
-
-
+            (original_img, img1, img2) = split_image(imgPath=file)
+            save_image(img=img1, original_img=original_img, fp=Path(domino_loc,'Xs',sf)/file.name)
+            save_image(img=img2, original_img=original_img, fp=Path(domino_loc,'Ys',sf)/file.name)
 
 
 
@@ -48,7 +73,7 @@ if __name__ == '__main__':
                         help="domino image folder containing the train, val and test folder e.g. ./data/maps")
     args = parser.parse_args()
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("split_domino")
     logger.setLevel(logging.DEBUG)
 
     split_domino(args.domino_loc)
---
+++ nbs/pix2pix.py
@@ -0,0 +1,214 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+# %%
+import fastai
+from fastai.vision import *
+
+# %% [markdown]
+# # Dataset Download
+# %% [markdown]
+# 
+
+# %%
+#!cd /storage && mkdir pix2pix && cd pix2pix && mkdir data
+
+get_ipython().system('cd .. && mkdir storage && cd storage && mkdir pix2pix && cd pix2pix && mkdir data')
+
+
+# %%
+
+
+
+# %%
+get_ipython().system('pip install wget')
+
+
+# %%
+# https://pypi.org/project/wget/
+# https://docs.python.org/2/library/tarfile.html
+import tarfile
+import wget
+
+downloaded_tars=list()
+def get_tar(url: str, dest: str):
+    filename = wget.download(url, dest)
+    downloaded_tars.append({'path':dest, 'filename':filename})
+    return filename
+
+def _opentar(fname: str):
+    tar=None
+    if (fname.find('.tar') and fname.endswith('.gz')):
+        tar = tarfile.open(fname, "r:gz")
+    elif (fname.endswith(".tar")):
+        tar = tarfile.open(fname, "r:")
+    return tar
+
+def untar(fname: str, dest:str='.'):
+    tar = _opentar(fname)
+    tar.list()
+    tar.extractall(path=dest)
+    tar.close()
+
+def viewtar(fname: str):
+    tar = _opentar(fname)
+    tar = tarfile.open(fname, "r:gz")
+    tar.list()
+    tar.close()
+
+def getuntarsize(fname: str):
+    tar = _opentar(fname)
+    tar = tarfile.open(fname, "r:gz")
+    # to do
+    tar.close()
+
+
+# %%
+wget.download('http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/maps.tar.gzhttp://efrosgans.eecs.berkeley.edu/pix2pix/datasets/maps.tar.gz', 'storage/pix2pix/data''storage/pix2pix/data')
+
+
+# %%
+url_dataset='http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/maps.tar.gz'
+# on gradient : dest='/storage/pix2pix/data'
+dest='storage/pix2pix/data'
+
+
+# %%
+## ONLY EXECUTE FIRST TIME to download the dataset tar and extract it all
+print(url_dataset)
+print(dest)
+filename=get_tar(url_dataset, dest)
+print(filename)
+print(dest)
+untar(filename, dest)
+
+
+# %%
+print(dest)
+os.listdir(dest)
+#!rm /storage/pix2pix/data/maps.tar (1).gz
+
+
+# %%
+for t in downloaded_tars:
+    print(t.filename)
+    print(t.path)
+
+
+# %%
+# Setting up dataset path for fastai
+path=Path(dest + '/maps')
+path
+
+# %% [markdown]
+# # PREPARING DATA FOR TRAINING
+# %% [markdown]
+# Each image will be loaded, rescaled, and split into the satellite and Google map elements. The result will be 1,097 color image pairs with the width and height of 256×256 pixels.
+
+# %%
+get_ipython().system('ls ../storage/pix2pix/data/maps')
+
+
+# %%
+# Set those variables if running independently from the data set download
+dest='../storage/pix2pix/data'
+path=Path(dest,'maps')
+pathXs=path/'Xs'
+pathYs=path/'Ys'
+
+
+# %%
+pathXs.is_dir()
+
+
+# %%
+il = ImageList.from_folder(pathXs, presort=True).split_by_folder(train='train', valid='val')
+type(il)
+
+
+
+# %%
+ImageImageList
+
+
+# %%
+iil = ImageImageList.from_folder(pathXs, presort=True).split_by_folder(train='train', valid='valid')
+
+
+# %%
+
+
+
+# %%
+iil.valid
+
+
+# %%
+iil.train
+
+
+# %%
+iil.items[10]
+
+
+# %%
+iil.valid.items[10]
+
+
+# %%
+On writing your won item list : https://docs.fast.ai/data_block.html#ItemList
+
+
+# %%
+iil.lists[0]
+
+
+# %%
+iil.lists[1]
+
+
+# %%
+iil.lists[1][10]
+
+
+# %%
+index=iil.train.items[0].parts.index('Xs')
+new_path = Path(*iil.train.items[0].parts[:index]).joinpath(Path('Ys',*iil.train.items[0].parts[index+1:]))
+print(new_path)
+type(iil.train.items[0])
+
+def getYsfromXs(x:Path, xFolder:str='Xs', yFolder:str='Ys'):
+    index=x.parts.index(xFolder)
+    newPath=Path(*x.parts[:index]).joinpath(Path(yFolder,*x.parts[index+1:]))
+    return newPath
+
+myPath=getYsfromXs(iil.train.items[1])
+print(myPath)
+
+
+# %%
+def get_data(iil:ImageImageList, bs:int,size:int):
+    ''' Takes an ImageImageList in iil and return a databunch with images of size=size and training batch size= bs '''
+    data = (iil.label_from_func(lambda x: pathYs/x.name)
+           .transform(get_transforms(max_zoom=2.), size=size, tfm_y=True)
+           .databunch(bs=bs).normalize(imagenet_stats, do_y=True))
+
+    data.c = 3
+    return data
+
+
+# %%
+bs = 16
+size = 100
+
+
+# %%
+data = get_data(iil=iil, bs=16,size=100)
+
+
+# %%
+
+
+
